78b36c635a360221d32ecd61d2707c201a522026
dimitar.d <dimitar.d@digipen.edu>
2021-12-04 00:53:36 -0800

    + Forgot non versioned files .-.

---
+++ assets/js/MovingSidebar.js
@@ -0,0 +1,64 @@
+var scrollPadding = 50;
+var entryPadding = 30;
+
+let sidebarEntries = ['#sidebar-about-me', '#sidebar-contact', '#sidebar-resumes', '#sidebar-portfolio', '#sidebar-education', '#sidebar-skills'];
+let anchorEntries = ['#anchor-about-me', '#anchor-contact', '#anchor-resumes', '#anchor-portfolio', '#anchor-education', '#anchor-skills'];
+
+// Apply positions to side bar elements based on anchor positions.
+document.addEventListener('DOMContentLoaded', function() {
+    for(let step = 0; step < sidebarEntries.length; step++)
+    {
+        $(sidebarEntries[step]).css("top", $(anchorEntries[step]).offset().top);
+    }
+    
+    scrollFunction();
+}, false);
+
+// Update positions on resizing window.
+window.addEventListener('resize', scrollFunction);
+
+// Set side bar elements to be fixed when passing anchor positions.
+$(window).scroll(function(){
+    scrollFunction();
+});
+
+function scrollFunction()
+{
+    for(let step = 0; step < sidebarEntries.length; step++)
+    {
+        var scrollTargetTop = determineScrollOffset(step, true);
+        var scrollTargetBottom = determineScrollOffset(step, false)
+        
+        if($(window).scrollTop() + scrollTargetTop > $(anchorEntries[step]).offset().top)
+        {            
+            $(sidebarEntries[step]).addClass('sidebar-entry-scrolling-1');
+            $(sidebarEntries[step]).css("top", scrollTargetTop);
+            //$(sidebarEntries[step]).css("background-color", "rgba(255,0,0,255)");
+        }
+        else if($(window).scrollTop() + $(window).height() - scrollTargetBottom < $(anchorEntries[step]).offset().top)
+        {
+            
+            $(sidebarEntries[step]).addClass('sidebar-entry-scrolling-1');
+            $(sidebarEntries[step]).css("top", $(window).height() - scrollTargetBottom);
+            //$(sidebarEntries[step]).css("background-color", "rgba(0,255,0,1)");
+        }
+        else
+        {
+            $(sidebarEntries[step]).removeClass('sidebar-entry-scrolling-1');
+            $(sidebarEntries[step]).css("top", $(anchorEntries[step]).offset().top);
+            //$(sidebarEntries[step]).css("background-color", "rgba(0,0,255,1)");
+        }
+    }
+}
+
+function determineScrollOffset(step, fromTheTop)
+{
+    if(fromTheTop)
+    {
+        return (step * entryPadding) + scrollPadding;
+    }
+    else
+    {
+        return (anchorEntries.length - step) * entryPadding + scrollPadding;
+    }
+}
